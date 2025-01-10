8a13d1fb54c6555513d21221d16f4a8aae057a44
Kofi Yeboah <kofiyeboah@proton.me>
2025-01-10 08:48:41 -0800

    ProgressIndicator implementation

--- lib/app/modules/details/details_view.dart
+++ lib/app/modules/details/details_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_todo/app/core/utils/extensions.dart';
 import 'package:getx_todo/app/modules/home/controller.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class DetailPage extends StatelessWidget {
   final homeController = Get.find<HomeController>();
@@ -12,59 +13,93 @@ class DetailPage extends StatelessWidget {
     var task = homeController.task.value;
     var color = HexColor.fromHex(task!.color!);
     return Scaffold(
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(3.0.wp),
-            child: Row(
-              children: [
-                IconButton(
-                    onPressed: () {
-                      Get.back();
-                    },
-                    icon: Icon(Icons.arrow_back))
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0.wp),
-            child: Row(
-              children: [
-                Icon(
-                  IconData(
-                    task.icon,
-                    fontFamily: 'MaterialIcons',
-                  ),
-                  color: color,
-                ),
-                SizedBox(width: 2.0.wp),
-                Text(
-                  task.title,
-                  style: TextStyle(
-                    fontSize: 12.0.sp,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Obx(() {
-            var totalTodos = homeController.doingTodos.length +
-                homeController.doneTodos.length;
-            return Padding(
-              padding:
-                  EdgeInsets.only(left: 16.0.wp, right: 16.0.wp, top: 3.0.wp),
+      body: Form(
+        key: homeController.formKey,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(3.0.wp),
               child: Row(
                 children: [
+                  IconButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      icon: Icon(Icons.arrow_back))
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0.wp),
+              child: Row(
+                children: [
+                  Icon(
+                    IconData(
+                      task.icon,
+                      fontFamily: 'MaterialIcons',
+                    ),
+                    color: color,
+                  ),
+                  SizedBox(width: 2.0.wp),
                   Text(
-                    '$totalTodos Tasks',
-                    style: TextStyle(fontSize: 12.0.sp, color: Colors.grey),
+                    task.title,
+                    style: TextStyle(
+                      fontSize: 12.0.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
-            );
-          })
-        ],
+            ),
+            Obx(
+              () {
+                var totalTodos = homeController.doingTodos.length +
+                    homeController.doneTodos.length;
+                return Padding(
+                  padding: EdgeInsets.only(
+                      left: 16.0.wp, right: 16.0.wp, top: 3.0.wp),
+                  child: Row(
+                    children: [
+                      Text(
+                        '$totalTodos Tasks',
+                        style: TextStyle(fontSize: 12.0.sp, color: Colors.grey),
+                      ),
+                      SizedBox(
+                        width: 3.0.wp,
+                      ),
+                      Expanded(
+                        child: StepProgressIndicator(
+                          totalSteps: totalTodos == 0 ? 1 : totalTodos,
+                          currentStep: homeController.doneTodos.length,
+                          size: 5,
+                          padding: 0,
+                          selectedGradientColor: LinearGradient(
+                              colors: [
+                                color.withValues(
+                                  alpha: 0.5,
+                                  red: 1.0,
+                                  blue: 0.8,
+                                  green: 0.9,
+                                ),
+                                color
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight),
+                          unselectedGradientColor: LinearGradient(
+                            colors: [Colors.grey[300]!, Colors.grey[300]!],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            TextFormField(
+              controller: homeController.addformController,
+            )
+          ],
+        ),
       ),
     );
   }
